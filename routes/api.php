c6ddd309751440939d8df872b3ff848d2f1f5983
Luis <luishidalgoa@outlook.es>
2025-02-11 13:53:18 +0100

    Api actualizada de profesores

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\ProfessorApi;
 use App\Http\Controllers\API\CompanyApi;
+use App\Http\Controllers\API\PorfessorApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('companys', CompanyApi::class);
-Route::apiResource('professors', ProfessorApi::class);
+Route::apiResource('professors', PorfessorApi::class);
 
